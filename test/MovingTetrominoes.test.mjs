3026a13bc625037e40ac1d52fbf1fd84efcb4c6e
hajame <harri.mehtala@helsinki.fi>
2022-02-24 15:28:26 +0200

    5 - red - move left

---
+++ test/MovingTetrominoes.test.mjs
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { Board } from "../src/Board.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+describe("Moving tetrominoes", () => {
+  let board;
+  beforeEach(() => {
+    board = new Board(10, 6);
+  });
+
+  describe("When a Tetromino is dropped", () => {
+    beforeEach(() => {
+      board.drop(Tetromino.T_SHAPE);
+    });
+
+    it("it can move left", () => {
+      board.moveLeft();
+      expect(board.toString()).to.equalShape(
+        `...T......
+         ..TTT.....
+         ..........
+         ..........
+         ..........
+         ..........`
+      );
+    });
+  });
+});

3a625af1dac4da0a92ab7265815257769da5f6e7
hajame <harri.mehtala@helsinki.fi>
2022-03-18 13:48:07 +0200

    ex 9 (start) - get score - green

---
+++ src/NintendoScore.mjs
@@ -0,0 +1,15 @@
+export class NintendoScore {
+  score;
+
+  constructor() {
+    this.score = 0;
+  }
+
+  getScore() {
+    return this.score;
+  }
+
+  toString() {
+    return this.score;
+  }
+}

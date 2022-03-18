4679dc1969b81081d9ba851060988b687af630aa
hajame <harri.mehtala@helsinki.fi>
2022-03-18 14:04:24 +0200

    ex 9 - score for one line clear - red

--- test/NintendoScore.test.mjs
+++ test/NintendoScore.test.mjs
@@ -1,6 +1,29 @@
 import { expect } from "chai";
 import { Board } from "../src/Board.mjs";
 import { NintendoScore } from "../src/NintendoScore.mjs";
+import { Tetromino } from "../src/Tetromino.mjs";
+
+function fallToBottom(board) {
+  for (let i = 0; i < 7; i++) {
+    board.tick();
+  }
+}
+
+function turnBlock180degrees(board) {
+  board.rotateRight();
+  board.rotateRight();
+}
+
+function moveRight(times, board) {
+  for (let i = 0; i < times + 1; i++) {
+    board.moveRight();
+  }
+}
+function moveLeft(times, board) {
+  for (let i = 0; i < times + 1; i++) {
+    board.moveLeft();
+  }
+}
 
 describe("Nintendo Score", () => {
   let score;
@@ -10,5 +33,26 @@ describe("Nintendo Score", () => {
 
   it("Score starts as 0", () => {
     expect(score.getScore()).to.equal(0);
+  });
+
+  describe("When rows are removed", () => {
+    let board;
+    it("1 row earns 40 points", () => {
+      board = new Board(9, 6);
+      score = new NintendoScore();
+      board.attachScoreObserver(score);
+
+      board.drop(Tetromino.T_SHAPE);
+      moveLeft(3, board);
+      fallToBottom(board);
+      board.drop(Tetromino.T_SHAPE);
+      board.moveRight();
+      fallToBottom(board);
+      board.drop(Tetromino.T_SHAPE);
+      moveRight(4, board);
+      fallToBottom(board);
+
+      expect(score.getScore()).to.equal(40);
+    });
   });
 });

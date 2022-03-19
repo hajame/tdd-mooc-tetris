fa5fa7dbd705cfc2a0abfdfada6ec93ded867f2f
hajame <harri.mehtala@helsinki.fi>
2022-03-19 17:01:48 +0200

    ex 10 - shuffle 4 items with 2 types - green

--- test/ShuffleBag.test.mjs
+++ test/ShuffleBag.test.mjs
@@ -16,14 +16,33 @@ describe("Shuffle bag", () => {
     });
 
     it("3 draws give 3 distinct objects", () => {
-      let list = new Array();
-      list.push(bag.draw());
-      list.push(bag.draw());
-      list.push(bag.draw());
+      let list = draw(bag, 3);
       expect(getDistinctTypes(list).length).to.equals(3);
     });
   });
+
+  describe("When only 4 tetrominoes of 2 types are inserted", () => {
+    beforeEach(() => {
+      bag.add(Tetromino.T_SHAPE, 1);
+      bag.add(Tetromino.O_SHAPE, 1);
+      bag.add(Tetromino.T_SHAPE, 1);
+      bag.add(Tetromino.O_SHAPE, 1);
+    });
+
+    it("4 draws give 2 distinct types", () => {
+      let list = draw(bag, 4);
+      expect(getDistinctTypes(list).length).to.equals(2);
+    });
+  });
 });
+
+function draw(bag, draws) {
+  let list = new Array();
+  for (let i = 0; i < draws; i++) {
+    list.push(bag.draw());
+  }
+  return list;
+}
 
 function getDistinctTypes(list) {
   return list
